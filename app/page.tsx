68cd8f6ffe1d72cd7f86951844b07f63426819a7
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-12-17 16:48:51 -0300

    feat: add funtionalities

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,46 @@
-import Image from 'next/image'
+import { DUMMY_POSTS } from '@/DUMMY_DATA'
+import CTACard from '@/components/elements/CTACard'
+import PaddingContainer from '@/components/layout/PaddingContainer'
+import PostCard from '@/components/post/PostCard'
+import PostList from '@/components/post/PostList'
+// import directus from '@/lib/directus'
+// import { notFound } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+
+  // const getAllPosts = async () => {
+  //   try {
+  //     const posts = await directus.items('posts').readByQuery({
+  //       fields: [
+  //         '*',
+  //         'author.id',
+  //         'author.first_name',
+  //         'author.last_name',
+  //         'category.id',
+  //         'category.title',
+  //       ]
+  //     })
+  //     return posts.data
+  //   } catch (error) {
+  //     throw new Error('Ooooops..')
+  //   }
+  // }
+
+  // const posts = await getAllPosts()
+
+  // if (!posts){
+  //   notFound()
+  // }
+
   return (
-    <main className="">
-
-    </main>
+    <PaddingContainer>
+      <main className="h-auto space-y-10">
+        <PostCard post={DUMMY_POSTS[0]}/>
+        <PostList posts={DUMMY_POSTS.filter((post, index) => index > 1 && index < 4 )} layout='vertical'/>
+        <PostCard post={DUMMY_POSTS[3]} reverse/>
+        <PostList posts={DUMMY_POSTS.filter((post, index) => index > 3 && index < 10 )} layout='vertical'/>
+        <CTACard />
+      </main>
+    </PaddingContainer>
   )
 }
